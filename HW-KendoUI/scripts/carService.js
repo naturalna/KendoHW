78080c7b5091bad83331889e6b8904df0a59987a
Anna Stoqnowa <naturalna89@gmail.com>
2013-10-09 15:44:04 -0500

    Changes made by Anna Stoqnowa (naturalna89@gmail.com).

--- HW-KendoUI/scripts/carService.js
+++ HW-KendoUI/scripts/carService.js
@@ -1,16 +1,104 @@
 (function (global) {
-    var CarViewModel,
-        app = global.app = global.app || {};
-
     
-  CarViewModel =  function CarViewModel(e) {
-    e.view.element.find("#listview").kendoMobileListView({ 
-        dataSource: new kendo.data.DataSource({
-                data: cars
-})
+     app = global.app = global.app || {};
+    
+   /* var viewModel = kendo.observable({
+        allCars:[],
     });
-};
-    app.carService = {
-        carViewModel: CarViewModel(e)
-    };
+    
+    function init(e) {
+        kendo.bind(e.view.element, viewModel);
+        var cars = [
+            { make: 'Opel', model: 'Insignia' , year: '2009', 
+            rent:{ 
+                status: 'false',    
+                options:{
+                            day:'true',
+                            mouth:'true',
+                            year:'false'
+                        }}},
+            { make: 'Audi', model: 'A5', year: '2008', 
+            rent:{ 
+                status: 'false',    
+                options:{
+                            day:'true',
+                            mouth:'true',
+                            year:'false'
+                        }}},
+            { make: 'BMW', model: 'M3', year: '2010', 
+            rent:{ 
+                status: 'false',    
+                options:{
+                            day:'true',
+                            mouth:'true',
+                            year:'false'
+                        }}},
+            { make: 'Mercedes', model: 'CL', year: '2011',
+            rent:{ 
+                status: 'false',    
+                options:{
+                            day:'true',
+                            mouth:'true',
+                            year:'false'
+                        }}},
+            ];
+
+            viewModel.set("allCars", cars);            
+               
+    }
+    
+    app.cars = {
+        init:init          
+    };*/
+    var cars = [
+            { make: 'Opel', model: 'Insignia' , year: '2009', 
+            rent:{ 
+                status: 'false',    
+                options:{
+                            day:'true',
+                            mouth:'true',
+                            year:'false'
+                        }}},
+            { make: 'Audi', model: 'A5', year: '2008', 
+            rent:{ 
+                status: 'false',    
+                options:{
+                            day:'true',
+                            mouth:'true',
+                            year:'false'
+                        }}},
+            { make: 'BMW', model: 'M3', year: '2010', 
+            rent:{ 
+                status: 'false',    
+                options:{
+                            day:'true',
+                            mouth:'true',
+                            year:'false'
+                        }}},
+            { make: 'Mercedes', model: 'CL', year: '2011',
+            rent:{ 
+                status: 'false',    
+                options:{
+                            day:'true',
+                            mouth:'true',
+                            year:'false'
+                        }}},
+            ];
+    
+    var ds = new kendo.data.DataSource({
+        data: cars,
+        columns: [
+                	{ field: 'make', width: 50, title: 'Make' },
+                	{ field: 'model', width: 50, title: 'Model' },
+                    { field: 'rent.status', width: 50, title: 'Free' },
+                    
+],
+
+
+});
+
+$("#listView-sourse").kendoMobileListView({
+    dataSource: ds
+});
+    
 })(window);
